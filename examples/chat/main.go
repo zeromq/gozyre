df74c6330ad700aa53ffa213a6f9fa341268af87
Michal Vyskocil <michal.vyskocil@gmail.com>
2019-03-21 12:14:57 +0100

    P: wrong go formatting
    S: run gofmt

--- examples/chat/main.go
+++ examples/chat/main.go
@@ -4,74 +4,74 @@ package main
 // Copyright (c) 2019 The GoZyre Authors
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 
-    zyre "github.com/zeromq/gozyre"
+	zyre "github.com/zeromq/gozyre"
 )
 
 func chatActor(pipe chan string, name string) {
 
-    node := zyre.New(
-        name,
-    )
-    node.Start()
-    err := node.Join("CHAT")
+	node := zyre.New(
+		name,
+	)
+	node.Start()
+	err := node.Join("CHAT")
 
-    zyreChan := make(chan interface{})
-    go func() {
-        for {
-            msg, err := node.Recv()
-            if err != nil {
-                panic(err)
-            }
-            zyreChan <-msg
-        }
-    }()
+	zyreChan := make(chan interface{})
+	go func() {
+		for {
+			msg, err := node.Recv()
+			if err != nil {
+				panic(err)
+			}
+			zyreChan <- msg
+		}
+	}()
 
-    if err != nil {
-        panic(err)
-    }
-    for {
-        select {
-        case msg := <-zyreChan:
-            switch msg.(type) {
-            case zyre.Join:
-                msg := msg.(zyre.Join)
-                fmt.Printf("%s has joined the chat\n", msg.Name)
-            case zyre.Exit:
-                msg := msg.(zyre.Exit)
-                fmt.Printf("%s has left the chat\n", msg.Name)
-            case zyre.Shout:
-                msg := msg.(zyre.Shout)
-                fmt.Printf("%s: %s\n", msg.Name, string(msg.Message[0]))
-            case zyre.Evasive:
-                msg := msg.(zyre.Evasive)
-                fmt.Printf("%s is evasive\n", msg.Name)
-            }
-        case shout := <-pipe:
-            if shout == "$TERM" {
-                break
-            }
-            node.ShoutString("CHAT", shout)
-        }
-    }
+	if err != nil {
+		panic(err)
+	}
+	for {
+		select {
+		case msg := <-zyreChan:
+			switch msg.(type) {
+			case zyre.Join:
+				msg := msg.(zyre.Join)
+				fmt.Printf("%s has joined the chat\n", msg.Name)
+			case zyre.Exit:
+				msg := msg.(zyre.Exit)
+				fmt.Printf("%s has left the chat\n", msg.Name)
+			case zyre.Shout:
+				msg := msg.(zyre.Shout)
+				fmt.Printf("%s: %s\n", msg.Name, string(msg.Message[0]))
+			case zyre.Evasive:
+				msg := msg.(zyre.Evasive)
+				fmt.Printf("%s is evasive\n", msg.Name)
+			}
+		case shout := <-pipe:
+			if shout == "$TERM" {
+				break
+			}
+			node.ShoutString("CHAT", shout)
+		}
+	}
 
-    node.Stop()
-    node.Destroy()
+	node.Stop()
+	node.Destroy()
 }
 
 func main() {
 
-    name := os.Args[1]
-    pipe := make(chan string)
-    go chatActor(pipe, name)
+	name := os.Args[1]
+	pipe := make(chan string)
+	go chatActor(pipe, name)
 
-    inp := bufio.NewScanner(os.Stdin)
-    for inp.Scan() {
-        pipe <- inp.Text()
-    }
+	inp := bufio.NewScanner(os.Stdin)
+	for inp.Scan() {
+		pipe <- inp.Text()
+	}
 
-    pipe <- "$TERM"
+	pipe <- "$TERM"
 }
